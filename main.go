5e3d556da33da1eceec84acfe2362e8409c306f5
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-09-07 07:53:03 +0700

    mengambil query string

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/",helloHandler)
+	mux.HandleFunc("/",homeHandler)
+	mux.HandleFunc("/produk", produkHandler)
+
+	log.Println("server berjalan di port 9000")
 	
 	err := http.ListenAndServe(":9000",mux)
 	log.Fatal(err)
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request)  {
+func homeHandler(w http.ResponseWriter, r *http.Request)  {
+	if r.URL.Path != "/"{
+		http.NotFound(w,r)
+		return
+	}
+
 	w.Write([]byte("<h1>tes aja</h1>"))
+}
+
+func produkHandler(w http.ResponseWriter, r *http.Request){
+
+	tangkap_id := r.URL.Query().Get("id")
+	id,err := strconv.Atoi(tangkap_id)
+
+	if err != nil || id < 1{
+		http.NotFound(w, r)
+	}
+
+	fmt.Fprintf(w,"Produk dengan id %d",id)
 }
