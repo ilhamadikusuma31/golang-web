f4be19e3042696e24092ac89f31a0b689d420b3b
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-09-14 22:54:11 +0700

    menggunakan static file

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ func main() {
 	mux.HandleFunc("/",handler.HomeHandler)
 	mux.HandleFunc("/produk", handler.ProdukHandler)
 
+
+	fileServer := http.FileServer(http.Dir("assets"))
+	mux.Handle("/static/",http.StripPrefix("/static",fileServer))
+
 	log.Println("server berjalan di port 9000")
 	fmt.Println("tes ajah")
 	
